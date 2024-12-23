7014c197dd2903fad64857e663c55fea570e2d07
Charuka1 <129839125+Charuka1@users.noreply.github.com>
2024-12-23 19:06:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,5 +12,5 @@ LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'JrdWz7X7JpkZs2YDhZ5ZVzyki6x5ZJ1Jkx4A' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'charuka1' : process.env.GITHUB_USER_NAME,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
-};
+
 };
